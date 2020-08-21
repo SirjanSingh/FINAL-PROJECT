87034894f0395d8ba4e5a348a04f3d96ed21728b
SirjanSingh <sirjan.singh036@gmail.com>
2020-08-21 19:19:29 +0530

    MADE THE WHOLE GAME

--- js/Player.js
+++ js/Player.js
@@ -3,6 +3,7 @@ class Player {
     this.index = null;
     this.distance = 0;
     this.name = null;
+    this.rank = null;
   }
 
   getCount(){
@@ -31,5 +32,16 @@ class Player {
     playerInfoRef.on("value",(data)=>{
       allPlayers = data.val();
     })
+  }
+   getCarsAtEnd(){
+    var getCarsAtEndRef = database.ref('carsAtEnd').on("value",(data)=>{
+      this.rank = data.val();
+    })
+  }
+
+  static updateCarsAtEnd(rank){
+  database.ref('/').update({
+    'carsAtEnd' : rank
+  })
   }
 }
