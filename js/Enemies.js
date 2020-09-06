3413e8000cc9ea9dacf62e9b14ded22a1681a069
SirjanSingh <sirjan.singh036@gmail.com>
2020-09-06 20:52:09 +0530

    corrected animation

--- js/Enemies.js
+++ js/Enemies.js
@@ -5,17 +5,17 @@ class Enemies {
 
     createEnemies(){
             var enemies = createSprite(random(50,450),0);
-            var rand = random(1,5);
+            var rand = Math.round(random(1,5));
             console.log(rand)
             switch(rand){
                 case 1:
-                    enemies.addImage(enemyCyanImg);
+                    enemies.addImage(enemyRedImg);
                     break;
                 case 2:
                     enemies.addImage(enemyGreenImg);
                     break;    
                 case 3:
-                    enemies.addImage(enemyRedImg);
+                    enemies.addImage(enemyCyanImg);
                     break;
                 case 4:
                     enemies.addImage(enemyYellowImg);
@@ -23,6 +23,8 @@ class Enemies {
                 case 5:
                     enemies.addImage(enemyWhiteImg);
                     break;
+                default :
+                    enemies.addImage(enemyRedImg)    
             }
             enemies.velocityY = 5;
             enemiesGroup.add(enemies);
