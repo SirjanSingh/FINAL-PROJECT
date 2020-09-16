fc301c422e028b84e6028f180648225cfef8e118
SirjanSingh <sirjan.singh036@gmail.com>
2020-09-16 14:01:42 +0530

    added p5.js

--- js/Game.js
+++ js/Game.js
@@ -102,7 +102,7 @@ class Game {
             frameRate(144)
         }*/
         //console.log(frameRate())
-<<<<<<< HEAD
+
         if(frameC % 15 === 0 && frameC > (fc3+75) && e1r < 5){
             //enemiesObj.createEnemies(random(50,450),0,0,6);
                 enemiesObj.enemiesRed1(0,120,5,0)
@@ -113,12 +113,12 @@ class Game {
            
             // console.log("wrdf\ndrgfc\nresdfc\neradsv\nhj")
                 enemiesObj.enemiesRed2(500,120,-5,0)
-            
-=======
+        }
+/*
         if(frameCount % 25 === 0){
             enemiesObj.createEnemies(random(50,450),0,0,5);
->>>>>>> 972d06d01f2a996cb517ea5cb88fb7b0d3383f7b
-        }
+
+        }*/
 
             enemiesObj.rotateE1r(0,5);
             enemiesObj.rotateE2r(0,5);
